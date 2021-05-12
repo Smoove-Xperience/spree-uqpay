a7300c64aea0998317b68b9c7e7ce6deded97ab5
helmiatwork <helmi.at.work@gmail.com>
2021-05-12 12:24:30 +0800

    refactor uqpay common

--- app/models/spree/uqpay_common.rb
+++ app/models/spree/uqpay_common.rb
@@ -1,111 +1,107 @@
 module Spree
-    module UqpayCommon
-      extend ActiveSupport::Concern
-      
-      included do        
-        preference :uqpay_host, :string
-        preference :uqpay_public_key, :string
-        preference :uqpay_private_key, :string
-        preference :uqpay_payment_merchant_id
-        preference :uqpay_callback_url
-        preference :uqpay_return_url
-
-        PAYMENT_URL = "#{uqpay_host}/pay"
-        CANCEL_URL = "#{uqpay_host}/cancel"
-        REFUND_URL = "#{uqpay_host}/refund"
+  module UqpayCommon
+    extend ActiveSupport::Concern
     
-        def payment_data          
-          {
-            'merchant_id': uqpay_payemnt_merchant_id,
-            'tranver_type': "pay",
-            'orderid': "",
-            'methodid': 2002,
-            'amount': "0.01",
-            'currency': "SGD",
-            'transname': "TopUp",
-            'quantity': "1",
-            'returnurl': '',
-            'callbackurl': uqpay_callback_url,
-            'date': 1611741568985,
-            'clienttype': "1",
-            'clientip': "22",
-            'signtype': "RSA",
-          }
+    included do        
+      preference :uqpay_host, :string        
+      preference :uqpay_private_key, :string
+      preference :uqpay_payment_merchant_id, :string
+      preference :uqpay_callback_url, :string
+      preference :uqpay_return_url, :string
+
+      PAYMENT_URL = "#{uqpay_host}/pay"
+      CANCEL_URL = "#{uqpay_host}/cancel"
+      REFUND_URL = "#{uqpay_host}/refund"
+  
+      def payment_data(purchase_params)          
+        data = {
+          'merchant_id': uqpay_payemnt_merchant_id,
+          'transtype': "pay",
+          # 'orderid': "",
+          # 'methodid': 2001,
+          # 'amount': "0.01",
+          # 'currency': "SGD",
+          'transname': "Purchase - #{purchase_params[:orderid]}",
+          # 'quantity': "1",
+          'returnurl': uqpay_return_url,
+          'callbackurl': uqpay_callback_url,
+          'date': Time.zone.now.to_i,
+          'clienttype': "1",
+          'clientip': request.remote_ip,
+          'signtype': "RSA",
+        }.merge(purchase_params)
+      end
+
+      def cancel_data(cancel_params)
+        data = {
+          'merchant_id': uqpay_payemnt_merchant_id,
+          'transtype': "cancel",
+          # 'orderid': "",
+          # 'uqorderid': "",            
+          # 'amount': "0.01",
+          'callbackurl': uqpay_callback_url,
+          'date': Time.zone.now.to_i,            
+          'clienttype': '1',
+          'signtype': 'RSA'         
+        }.merge(cancel_params)
+      end
+
+      def refund_data(refund_params)
+        {
+          'merchant_id': uqpay_payemnt_merchant_id,
+          'transtype': "refund",
+          # 'orderid': "",
+          # 'uqorderid': "",            
+          # 'amount': "0.01",
+          'date': Time.zone.now.to_i,
+          'callbackurl': uqpay_callback_url,
+          'clienttype': '1',
+          'signtype': 'RSA'                      
+        }.merge(refund_params)
+      end
+
+      def create_signature(data)
+        data = data.sort_by { |key| key }.reverse.to_h
+
+        digest = OpenSSL::Digest::SHA1.new
+        pkey = OpenSSL::PKey::RSA.new uqpay_private_key
+        signature = pkey.sign(digest, data)
+        
+        Base64.strict_encode64(signature)
+      end
+
+      def verify_signature(data)
+        data = data.sort_by { |key| key }.reverse.to_h
+
+        digest = OpenSSL::Digest::SHA1.new
+        pkey = OpenSSL::PKey::RSA.new uqpay_private_key
+        pub_key = pkey.public_key
+        
+        pub_key.verify(digest, signature, data)
+      end
+
+      def pay
+        send(PAYMENT_URL, payment_data)          
+      end
+
+      def cancel
+        send(CANCEL_URL, cancel_data)
+      end
+
+      def refund
+        send(REFUND_URL, refund_data)
+      end
+
+      private
+      def send(url, data)
+        resp = Faraday.post(url) do |req|
+          req.headers['Accept'] = 'application/json'
+          req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+
+          req.params = data.merge{'sign': create_signature(data)}
         end
 
-        def cancel_data
-          {
-            'merchant_id': uqpay_payemnt_merchant_id,
-            'tranver_type': "pay",
-            'orderid': "",
-            'uqorderid': "",            
-            'amount': "0.01",
-            'date': '',
-            'callbackurl': uqpay_callback_url,
-            'clienttype': '',
-            'signtype': 'RSA'         
-          }
-        end
-
-        def refund_data
-          {
-            'merchant_id': uqpay_payemnt_merchant_id,
-            'tranver_type': "pay",
-            'orderid': "",
-            'uqorderid': "",            
-            'amount': "0.01",
-            'date': '',
-            'callbackurl': uqpay_callback_url,
-            'clienttype': '',
-            'signtype': ''                      
-          }
-        end
-
-        def create_signature(data)
-          digest = OpenSSL::Digest::SHA256.new
-          pkey = OpenSSL::PKey::RSA.new(2048)
-          signature = pkey.sign(digest, data)
-          
-          signature
-        end
-
-        def verify_signature
-          pub_key = pkey.public_key
-          pub_key.verify(digest, signature, data) # => true
-        end
-
-        def pay
-          resp = Faraday.post(PAYMENT_URL) do |req|
-            req.headers['Accept'] = 'application/json'
-            req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-
-            req.params = payment_data << {'sign': create_signature(payment_data)}
-          end
-
-          resp
-        end
-
-        def cancel
-          resp = Faraday.post(CANCEL_URL) do |req|
-            req.headers['Accept'] = 'application/json'
-            req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-
-            req.params = cancel_data << {'sign': create_signature(cancel_data)}
-          end
-
-          resp
-        end
-
-        def refund
-          resp = Faraday.post(PAYMENT_URL) do |req|
-            req.headers['Accept'] = 'application/json'
-            req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-
-            req.params = refund_data << {'sign': create_signature(refund_data)}
-          end
-
-          resp
-        end
+        resp
       end
     end
   end
